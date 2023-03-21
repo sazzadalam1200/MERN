484d19b440152df16fccec9c003808bffb1d7214
Sazzad Alam <baazifly@gmail.com>
2023-03-21 11:52:18 +0600

    js problems

---
+++ Module 1/problem.js
@@ -0,0 +1,47 @@
+//#1.print 1 to 10
+// for(let i=1;i<11;i++){
+//     console.log(i)
+// }
+
+//#2. Coding challenge #2: Print the odd numbers less than 100
+// for(let i= 1;i<100;i=i+2){
+//     console.log(i)
+// }
+
+//#3: Print the multiplication table with 7
+// for(i=1;i<11;i++){
+//     console.log("7 * "+i+" = "+ 7*i)
+// }
+
+//#4: Print all the multiplication tables with numbers from 1 to 10
+// function printTable(n){
+//     for(let i = 1; i<11;i++){
+//         var table = n + " * "+ i + " = "+ n*i
+//         console.log(table)
+//     }
+// }
+// for(i=1;i<11;i++){
+//     printTable(i)
+//     console.log("")
+// }
+
+//#5: Calculate the sum of numbers from 1 to 10
+// let sum = 0
+// function add(n){
+//     for(let i=1;i<=n;i++){
+//         sum = sum+i
+//     }
+//     console.log(sum)
+// }
+// add(10)
+
+//#6: Calculate 10!
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//       result *= i;
+//     }
+//     console.log(result)
+//   }
+// factorial(10)
+
