a6cc4a3f87201b38d7f1fad7ba10d49d6b07a380
Sazzad Alam <baazifly@gmail.com>
2023-03-22 19:53:28 +0600

    assignment modification

--- Module 1/assignment.js
+++ Module 1/assignment.js
@@ -1,8 +1,16 @@
 function multiplicationTable(n){
-    for(let i = 1; i<11;i++){
-        var table = n + " * "+ i + " = "+ n*i
-        console.log(table)
+   
+    if(Number.isInteger(n)){
+        for(let i = 1; i<11;i++){
+            var table = n + " * "+ i + " = "+ n*i
+            console.log(table)  
+        }
     }
+    else{
+        console.log("not integer")
+    }
+    
 }
 multiplicationTable(5)
-multiplicationTable(8)
+multiplicationTable(8.2)
+multiplicationTable(-3)
