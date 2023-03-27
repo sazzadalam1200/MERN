d59f807b1e6e268c206ddeb1fa9e8d7b8267e330
Sazzad Alam <baazifly@gmail.com>
2023-03-27 11:47:53 +0600

    module 2

--- Module 1/problem.js
+++ Module 1/problem.js
@@ -157,20 +157,139 @@
 // console.log(isPrime(37))
 
 //#17: Calculate the sum of digits of a positive integer number
+// function sumOfDigits(num){
+//     let sum = 0
+//     while(num>0){
+//         sum += num%10 //gets the last digit
+//         num = Math.floor(num/10) //removes the last digit
+//     }
+//     console.log(sum)
+// }
+// sumOfDigits(192)
 
 //#18: Print the first 100 prime numbers
+// function isPrime(n){
+//     if(n<2){
+//         return false
+//     }
+//     for(let i = 2; i<= Math.sqrt(n); i++){
+//         if(n%i===0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+// let count = 0 
+// let n = 2
+// while(count<100){
+//     if(isPrime(n)){
+//         console.log(n)
+//         count++
+//     }
+//     n++
+// }
 
 //#19: Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
 
+
 //#20: Rotate an array to the left 1 position
+// arr1 = [1,2,3,4,5,6,7]
+// function rotateLeft(ar){
+//     var first = ar.shift();
+//     ar.push(first);
+// }
+// rotateLeft(arr1)
+// console.log(arr1)
 
 //#21: Rotate an array to the right 1 position
+// let arr1 = [1,2,3,4,5,6,7]
+// function rotateRight(ar){
+//     var last = ar.pop(ar)
+//     ar.unshift(last) 
+// }
+// rotateRight(arr1)
+// console.log(arr1)
 
 //#22: Reverse an array
+// arr1 = [1,2,3,4,5,6,7]
+
+// function reverseArr(ar){
+//     let ar2 = []
+//     for(let i=ar.length-1; i>=0;i--){
+//         ar2.push(ar[i])
+//     }
+//     console.log(ar2)
+// }
+// reverseArr(arr1)
 
 //#23: Reverse a string
 
+// var string = "Javascript"
+// function reverseString(s){
+//     let s2 = ''
+//     for(let i = s.length-1;i>=0;i--){
+//         s2 += s[i]
+//     }
+//     return s2
+// }
+// console.log(reverseString(string))
+
 //#24: Create a function that will merge two arrays and return the result as a new array
+// var arr1 = [1,2,3]
+// var arr2 = [4,5,6]
+
+// function mergeArr(ar1,ar2){
+//     let ar = ar1.concat(ar2)
+//     return ar
+// }
+// console.log(mergeArr(arr1,arr2))
+
+//#25: Create a function that will receive two arrays of numbers as arguments and return an array composed
+// of all the numbers that are either in the first array or second array but not in both
+// var arr1 = [1, 2, 3, 10, 5, 4, 14]
+// var arr2 = [1, 4, 5, 6, 14]
+
+// function merge(ar1,ar2){
+//     var ar = []
+//     for (let el of ar1){
+//         if(!ar2.includes(el)){
+//             ar.push(el)
+//         }
+//     }
+//     for (let el of ar2) {
+//         if (!ar1.includes(el)) {
+//             ar.push(el);
+//         }
+//     }
+//     return ar
+// }
+// console.log(merge(arr1,arr2))
+
+//#26: Create a function that will receive two arrays and will return an array with elements that are in 
+//the first array but not in the second
+// var arr1 = [1, 2, 3, 10, 5, 4, 14]
+// var arr2 = [1, 4, 5, 6, 14]
+
+// function merge(ar1,ar2){
+//     var ar = []
+//     for (let el of ar1){
+//         if(!ar2.includes(el)){
+//             ar.push(el)
+//         }
+//     }
+//     return ar
+// }
+// console.log(merge(arr1,arr2))
 
 
+//#27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+
+//#28: Calculate the sum of first 100 prime numbers
+
+//#29: Print the distance between the first 100 prime numbers
+
+//#30: Create a function that will add two positive numbers of indefinite size. The numbers are received
+// as strings and the result should be also provided as string.
+
+//grade calculator
 
