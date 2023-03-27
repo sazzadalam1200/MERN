a0e201661b65cfa253669e62b2ea626ed24a9e23
Sazzad Alam <baazifly@gmail.com>
2023-03-27 13:22:41 +0600

    dom

--- Module 1/problem.js
+++ Module 1/problem.js
@@ -283,6 +283,27 @@
 
 
 //#27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+// let arr1 = [2,3,6,8,6,2,10,23,34]
+// function disticntElements(ar){
+//     let ar2 = []
+//     for(let i = 0; i<ar.length; i++){
+//         if(!isInArray(ar[i],ar2)){
+//             ar2.push(ar[i])
+//         }
+//     }
+//     console.log(ar2)
+// }
+
+// function isInArray(n,ar){
+//     for(let i = 0; i<ar.length; i++){
+//         if(ar[i]===n){
+//             return true
+//         }
+//     }
+//     return false
+// }
+// disticntElements(arr1)
+
 
 //#28: Calculate the sum of first 100 prime numbers
 
@@ -291,5 +312,4 @@
 //#30: Create a function that will add two positive numbers of indefinite size. The numbers are received
 // as strings and the result should be also provided as string.
 
-//grade calculator
 
