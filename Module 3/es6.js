cf9e648998e47141489a0403d1c07a76766c7a39
Sazzad Alam <baazifly@gmail.com>
2023-04-05 13:10:54 +0600

    es6

---
+++ Module 3/es6.js
@@ -0,0 +1,24 @@
+//spread operator
+// let alph1 = ['a','b','c','d','e']
+// let alph2 = ['f','g','h','i']
+// let alph3 = [...alph1,'f','g','h','i']
+// console.log(alph2)
+
+//rest parameter
+// function add(...num){
+//     let sum = 0
+//    for(let i of num){
+//     sum=sum+i
+//    }
+//     console.log(sum)
+// }
+
+// add(1,2,3,4,5,6,7,8,9)
+
+//Multidimensionalarray
+// let arr1 = ['gg','wp','nt','1hp']
+// let arr2 = ['ak','m4','famas','galil']
+// let arr3 = ['5-7','Deagle','usp-s','p250','glock']
+
+// let csgo=[arr1,arr2,arr3]
+// console.log(csgo[1][1])
